2900337e7e40c9cfa1abcc877f4381a9a9938ca1
Manuel Crisostomo <marcos.crisostomo@ids.com.mx>
2021-05-06 22:23:48 -0500

    Manejo Errores

--- clientes-back/src/main/java/com/clientes/controllers/ClienteRestController.java
+++ clientes-back/src/main/java/com/clientes/controllers/ClienteRestController.java
@@ -3,12 +3,15 @@ package com.clientes.controllers;
 import com.clientes.models.entity.Cliente;
 import com.clientes.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
@@ -23,4 +26,51 @@ public class ClienteRestController {
 		return clienteService.findAll();
 	}
 
+	@GetMapping("/clientes/{id}")
+	public ResponseEntity<?> show(@PathVariable Long id) {
+
+		Map<String, Object> response = new HashMap<>();
+		Cliente cliente;
+
+		try {
+			cliente = this.clienteService.findById(id);
+		} catch (DataAccessException e){
+			response.put("mensaje", "Error al realizar la consulta en BD!");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		if(cliente == null) {
+			response.put("mensaje", "El cliente ID: ".concat(id.toString()).concat(" no existe en la BD!"));
+			return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<>(cliente, HttpStatus.OK);
+	}
+
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		cliente.setCreateAt(new Date());
+		this.clienteService.save(cliente);
+		return cliente;
+	}
+
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente currentCliente = this.clienteService.findById(id);
+		currentCliente.setNombre(cliente.getNombre());
+		currentCliente.setApellido(cliente.getApellido());
+		currentCliente.setEmail(cliente.getEmail());
+		this.clienteService.save(currentCliente);
+		return currentCliente;
+	}
+
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		Cliente currentCliente = this.clienteService.findById(id);
+		this.clienteService.delete(currentCliente);
+	}
 }
